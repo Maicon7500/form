1b79a5f24e6145007ff04ea218a4e0fa877bb940
Maicon Santos <re.maicon77@gmail.com>
2024-04-30 10:37:39 -0300

    Adicionando JS

---
+++ script.js
@@ -0,0 +1,15 @@
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == 'admin' && senha == 'admin'){
+        alert('Sucesso');
+
+    }else{
+        alert('Usuário ou senha incorretos');
+    }
+
+}
+
+
